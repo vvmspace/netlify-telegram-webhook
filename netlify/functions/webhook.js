16dc055e578e0730c6d778c234738c9d5ee4c787
Vladimir V.M <vvmyagdeev@gmail.com>
2024-06-16 06:11:49 +0400

    feat<reply>: body (test)

--- netlify/functions/webhook.js
+++ netlify/functions/webhook.js
@@ -1,12 +1,50 @@
 // /.netlify/functions/webhook
 
 exports.handler = async (event, context) => {
-    console.log('event', event);
-    console.log('context', context);
+  console.log("event", event);
+  console.log("context", context);
+  const bodyAsText = event.body;
+  if (!bodyAsText) {
+    return {
+      statusCode: 400,
+      body: "No body provided",
+    };
+  }
+
+  const body = JSON.parse(bodyAsText);
+  const {
+    update_id,
+    message: {
+      message_id,
+      from: { id, is_bot, first_name, username, language_code },
+      chat: {
+        id: chatId,
+        first_name: chatFirstName,
+        username: chatUsername,
+        type: chatType,
+      },
+      date,
+      text,
+      entities,
+    },
+  } = body;
+
+  console.log(body);
+
+  // reply to the user
+    const response = {
+        method: "sendMessage",
+        chat_id: chatId,
+        text: `Hello, ${chatFirstName}!`,
+    };
 
     return {
         statusCode: 200,
-        body: JSON.stringify({ event, context}, null, 2)
+        body: JSON.stringify(response),
     };
-}
-    
+
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify({ event, context }, null, 2),
+//   };
+};
